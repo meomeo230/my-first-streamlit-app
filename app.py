84276568e317ef17d96a4876f9059cc742b9fbcb
meomeo230 <125683733+meomeo230@users.noreply.github.com>
2023-03-11 10:40:38 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ b = st.number_input('Tham số b')
 st.title('GIẢI PHƯƠNG TRÌNH BẬC NHẤT')
 
 if st.button('Giải'):
-    if (a==0 and b==0) st.success('Phương trình có vô số nghiệm)
-    if (a==0 and b!=0) st.success('Phương trình vô nghiệm')                              
-    if (a!=0) st.success('Phương trình có 1 nghiệm', -b/a)
+    if (a==0 and b==0): st.success('Phương trình có vô số nghiệm)
+    if (a==0 and b!=0): st.success('Phương trình vô nghiệm')                              
+    if (a!=0): st.success('Phương trình có 1 nghiệm', -b/a)
